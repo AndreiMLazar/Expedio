03ccd7ad2c7f6e3f0295e0e2fe9a47c6e364de76
Andrei Lazar <l.andrei37@yahoo.ro>
2019-06-06 01:28:59 +0300

    Production Mode Added

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv-safe");
+
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+} else {
+  require("dotenv-safe").config();
+}
+
 
 dotenv.config();
 
