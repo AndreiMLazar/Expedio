7b3ba2e0f04b38fe0562249be9d612603316bb8e
Andrei Lazar <l.andrei37@yahoo.ro>
2019-06-06 04:21:34 +0300

    Heroku Test

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 // Models
 const Ex = require("./models/ex");
